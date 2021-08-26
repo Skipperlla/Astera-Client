fa1160c385a426e1ff57efbb210686e073e29fa5
Ömer Esmer <68515357+Skipperlla@users.noreply.github.com>
2021-08-26 15:24:17 +0300

    Update userActions.ts

--- src/store/actions/userActions.ts
+++ src/store/actions/userActions.ts
@@ -5,7 +5,7 @@ export const login =
   (credentials: LoginForm) => async (dispatch: UserDispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-      const response = await api().post<User>("/Account/Login", credentials);
+      const response = await api().post<User>("/Account/Login",{ credentials, timeout: 1000});
       dispatch({ type: "LOGIN_SUCCESS", payload: response.data.data });
       Cookies.set("token", response.data.data.token);
       localStorage.setItem("authUser", JSON.stringify(response.data.data));
